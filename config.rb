29407ce613e4fb15898bbc1f59ce5dc584bc15ea
Samuel Overington <chaos@samueloverington.com>
2014-11-13 23:18:11 +0000

    added compass, changed format to make style.css export at ./ rather than /

--- config.rb
+++ config.rb
@@ -1,10 +1,11 @@
 require 'compass/import-once/activate'
 # Require any additional compass plugins here.
+require 'susy'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "stylesheets"
-css_path = "/"
+css_path = "./"
 sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "javascripts"
