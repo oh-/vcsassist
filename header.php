210c41ce96e565b74dac25bd348c1a1b994b503e
Samuel Overington <chaos@samueloverington.com>
2014-12-04 21:55:20 +0100

    added page shaddow effect

--- header.php
+++ header.php
@@ -31,6 +31,7 @@ Compass CSS files added below
 
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed site">
+<div id="page-wrap">
 	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', '_s' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
