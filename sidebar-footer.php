efd7235604dadd53d927993ce28013970d3a0c8c
Samuel Overington <chaos@samueloverington.com>
2015-03-19 12:18:01 +0000

    added footer sidebar option to display a widget or the info in sidebar-footer.php added cityscape-monocolour.jpg

--- sidebar-footer.php
+++ sidebar-footer.php
@@ -8,6 +8,21 @@
 		<div class="threecol">
 			<img src="<?php bloginfo( 'template_url' ); ?>/img/MAYOR-LOGO-CMYK.png" width="191" height="135" alt="MAYOR LOGO CMYK">
 			<img src="<?php bloginfo( 'template_url' ); ?>/img/esf_logo_rgb_45mm_lores.png" width="128" height="133" alt="Esf Logo Rgb 45mm Lores">
-			<img src="<?php bloginfo( 'template_url' ); ?>/img/lvscandunitedwayfooterlogo.png" width="200" height="55" alt="Lvscandunitedwayfooterlogo"></div>
+			<img src="<?php bloginfo( 'template_url' ); ?>/img/lvscandunitedwayfooterlogo.png" width="200" height="55" alt="Lvscandunitedwayfooterlogo">
+			<img src="<?php bloginfo( 'template_url' ); ?>/img/hi_big_e_lrg_pink-e1423675905446.jpg" width="150" height="112" alt="Hi Big E Lrg Pink E1423675905446">
+		</div>
+	</aside><!-- /#footer-space -->
+<?php else : ?>
+	<aside id="footer-space" class="footer-space widget-area site-footer" role="complementary">
+		<div class="twocol">
+			<img src="<?php bloginfo( 'template_url' ); ?>/img/lvscandunitedwayfooterlogo.png" width="200" height="55" alt="Lvscandunitedwayfooterlogo">
+		<p>VCS Assist is a project of London Voluntary Service Council (LVSC). It is part-financed by the European Social Fund (ESF), Trust for London, and Big Lottery Fund.  ESF invests in jobs and skills, focusing on people who need support the most.</p>
+		</div>
+		<div class="threecol">
+			<img src="<?php bloginfo( 'template_url' ); ?>/img/MAYOR-LOGO-CMYK.png" width="191" height="135" alt="MAYOR LOGO CMYK">
+			<img src="<?php bloginfo( 'template_url' ); ?>/img/esf_logo_rgb_45mm_lores.png" width="128" height="133" alt="Esf Logo Rgb 45mm Lores">
+			<img src="<?php bloginfo( 'template_url' ); ?>/img/lvscandunitedwayfooterlogo.png" width="200" height="55" alt="Lvscandunitedwayfooterlogo">
+			<img src="<?php bloginfo( 'template_url' ); ?>/img/hi_big_e_lrg_pink-e1423675905446.jpg" width="150" height="112" alt="Hi Big E Lrg Pink E1423675905446">
+		</div>
 	</aside><!-- /#footer-space -->
 <?php endif; ?>
