4aef2986cf97804c0b7533aa77386fbd4a89b509
Samuel Overington <chaos@samueloverington.com>
2015-03-19 13:09:15 +0000

    footer fixed with text fitting

--- sidebar-footer.php
+++ sidebar-footer.php
@@ -1,28 +1,16 @@
 <?php if ( is_active_sidebar( 'footer-space' ) ) : ?>
 	<aside id="footer-space" class="footer-space widget-area site-footer" role="complementary">
 		<?php dynamic_sidebar( 'footer-space' ); ?>
-		<div class="twocol">
-			<img src="<?php bloginfo( 'template_url' ); ?>/img/lvscandunitedwayfooterlogo.png" width="200" height="55" alt="Lvscandunitedwayfooterlogo">
-		<p>VCS Assist is a project of London Voluntary Service Council (LVSC). It is part-financed by the European Social Fund (ESF), Trust for London, and Big Lottery Fund.  ESF invests in jobs and skills, focusing on people who need support the most.</p>
-		</div>
-		<div class="threecol">
-			<img src="<?php bloginfo( 'template_url' ); ?>/img/MAYOR-LOGO-CMYK.png" width="191" height="135" alt="MAYOR LOGO CMYK">
-			<img src="<?php bloginfo( 'template_url' ); ?>/img/esf_logo_rgb_45mm_lores.png" width="128" height="133" alt="Esf Logo Rgb 45mm Lores">
-			<img src="<?php bloginfo( 'template_url' ); ?>/img/lvscandunitedwayfooterlogo.png" width="200" height="55" alt="Lvscandunitedwayfooterlogo">
-			<img src="<?php bloginfo( 'template_url' ); ?>/img/hi_big_e_lrg_pink-e1423675905446.jpg" width="150" height="112" alt="Hi Big E Lrg Pink E1423675905446">
-		</div>
 	</aside><!-- /#footer-space -->
 <?php else : ?>
 	<aside id="footer-space" class="footer-space widget-area site-footer" role="complementary">
-		<div class="twocol">
-			<img src="<?php bloginfo( 'template_url' ); ?>/img/lvscandunitedwayfooterlogo.png" width="200" height="55" alt="Lvscandunitedwayfooterlogo">
-		<p>VCS Assist is a project of London Voluntary Service Council (LVSC). It is part-financed by the European Social Fund (ESF), Trust for London, and Big Lottery Fund.  ESF invests in jobs and skills, focusing on people who need support the most.</p>
-		</div>
-		<div class="threecol">
-			<img src="<?php bloginfo( 'template_url' ); ?>/img/MAYOR-LOGO-CMYK.png" width="191" height="135" alt="MAYOR LOGO CMYK">
-			<img src="<?php bloginfo( 'template_url' ); ?>/img/esf_logo_rgb_45mm_lores.png" width="128" height="133" alt="Esf Logo Rgb 45mm Lores">
-			<img src="<?php bloginfo( 'template_url' ); ?>/img/lvscandunitedwayfooterlogo.png" width="200" height="55" alt="Lvscandunitedwayfooterlogo">
-			<img src="<?php bloginfo( 'template_url' ); ?>/img/hi_big_e_lrg_pink-e1423675905446.jpg" width="150" height="112" alt="Hi Big E Lrg Pink E1423675905446">
-		</div>
+		<div class="partner-logos">
+			<img src="<?php bloginfo( 'template_url' ); ?>/img/lvscandunitedwayfooterlogo.png" id="logo-lvsc-unitedway" alt="Lvscandunitedwayfooterlogo">
+			<img src="<?php bloginfo( 'template_url' ); ?>/img/MAYOR-LOGO-CMYK.png" id="logo-MOL" class="logo" alt="MAYOR LOGO CMYK">
+			<img src="<?php bloginfo( 'template_url' ); ?>/img/esf_logo_rgb_45mm_lores.png" id="logo-esf" class="logo" alt="Esf Logo Rgb 45mm Lores">
+			<img src="<?php bloginfo( 'template_url' ); ?>/img/hi_big_e_lrg_pink-e1423675905446.jpg" id="logo-lottery" class="logo" alt="Hi Big E Lrg Pink E1423675905446">
+		</div> <!-- /.partner-logos -->
+		<div id="lvsc-info"><p>VCS Assist is a project of London Voluntary Service Council (LVSC). It is part-financed by the European Social Fund (ESF), Trust for London, and Big Lottery Fund.  ESF invests in jobs and skills, focusing on people who need support the most.</p></div>
+
 	</aside><!-- /#footer-space -->
 <?php endif; ?>
