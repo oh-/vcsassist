ca4f007855f99e6db9046166134944667a8f4597
Samuel Overington (neon) <so@madeso.uk>
2015-09-15 18:43:29 +0100

    added old php stuff - to test now if it works

--- vcsassist-plugin.php
+++ vcsassist-plugin.php
@@ -1,408 +1,76 @@
 <?php
-/*
-Plugin Name: VCS Assist Site Plugin for vcsassist.org.uk
-Description: Site specific code changes for vcsassist.org.uk Features - [from Mrinal Kanti Roy's widget 'Category Posts Widget'] Adds a widget that shows the most recent posts from a single category.
-Author: madeso
-Version: 0.1
-Author URI: http://www.madeso.uk/
-*/
-/* widgets */
-
-
-// Don't call the file directly
-if ( !defined( 'ABSPATH' ) ) exit;
 
 /**
- * Register our styles
+ * The plugin bootstrap file
  *
- * @return void
- */
-add_action( 'wp_enqueue_scripts', 'category_posts_widget_styles' );
-
-function category_posts_widget_styles() {
-	wp_register_style( 'category-posts', plugins_url( 'vcsassist-plugin/stylesheets/screen.css' ) );
-	wp_enqueue_style( 'category-posts' );
-}
-
-/**
- * Register thumbnail sizes.
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
  *
- * @return void
- */
-function category_posts_add_image_size(){
-	$sizes = get_option('mkrdip_cat_post_thumb_sizes');
-	
-	if ( $sizes ){
-		foreach ( $sizes as $id=>$size ) {
-			add_image_size( 'cat_post_thumb_size' . $id, $size[0], $size[1], true );
-		}
-	}
-}
-
-add_action( 'init', 'category_posts_add_image_size' );
-
-
-/**
- * Category Posts Widget Class
+ * @link              http://example.com
+ * @since             1.0.0
+ * @package           Plugin_Name
  *
- * Shows the single category posts with some configurable options
+ * @wordpress-plugin
+ * Plugin Name: VCS Assist Site Plugin for vcsassist.org.uk
+ * Plugin URI:        http://example.com/vcsassist-plugin-uri/
+ * Description: Site specific code changes for vcsassist.org.uk Features - [from Mrinal Kanti Roy's widget 'Category Posts Widget'] Adds a widget that shows the most recent posts from a single category.
+ * Author: madeso
+ * Version: 0.1
+ * Author URI: http://www.madeso.uk/
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       vcsassist-plugin
+ * Domain Path:       /languages
  */
 
-
-
-function new_excerpt_more( $more ) {
-	return ' <a class="read-more" href="'. get_permalink( get_the_ID() ) . '">' . __('[Read more]', 'your-text-domain') . '</a>';
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
 }
-add_filter( 'excerpt_more', 'new_excerpt_more' );
-
-
 
 /**
- * Category Posts Widget Class
+ * The code that runs during plugin activation.
+ * This action is documented in includes/class-vcsassist-plugin-activator.php
+ */
+function activate_plugin_name() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-vcsassist-plugin-activator.php';
+	Plugin_Name_Activator::activate();
+}
+
+/**
+ * The code that runs during plugin deactivation.
+ * This action is documented in includes/class-vcsassist-plugin-deactivator.php
+ */
+function deactivate_plugin_name() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-vcsassist-plugin-deactivator.php';
+	Plugin_Name_Deactivator::deactivate();
+}
+
+register_activation_hook( __FILE__, 'activate_plugin_name' );
+register_deactivation_hook( __FILE__, 'deactivate_plugin_name' );
+
+/**
+ * The core plugin class that is used to define internationalization,
+ * admin-specific hooks, and public-facing site hooks.
+ */
+require plugin_dir_path( __FILE__ ) . 'includes/class-vcsassist-plugin.php';
+include_once plugin_dir_path( __FILE__ ) . 'includes/class-catposts.php';
+
+/**
+ * Begins execution of the plugin.
  *
- * Shows the single category posts with some configurable options
- */
-
-class CategoryPost extends WP_Widget {
-
-	function CategoryPost() {
-		$widget_ops = array('classname' => 'cat-post-widget', 'description' => __('List single category posts'));
-		$this->WP_Widget('category-posts', __('Category Posts'), $widget_ops);
-	}
-
-	// Displays category posts widget on blog.
-	function widget($args, $instance) {
-		global $post;
-		$post_old = $post; // Save the post object.
-		
-		extract( $args );
-		
-		$sizes = get_option('mkrdip_cat_post_thumb_sizes');
-		
-		// If not title, use the name of the category.
-		if( !$instance["title"] ) {
-			$category_info = get_category($instance["cat"]);
-			$instance["title"] = $category_info->name;
-	  }
-
-	  $valid_sort_orders = array('date', 'title', 'comment_count', 'rand');
-	  if ( in_array($instance['sort_by'], $valid_sort_orders) ) {
-		$sort_by = $instance['sort_by'];
-		$sort_order = (bool) isset( $instance['asc_sort_order'] ) ? 'ASC' : 'DESC';
-	  } else {
-		// by default, display latest first
-		$sort_by = 'date';
-		$sort_order = 'DESC';
-	  }
-		
-		// Get array of post info.
-	  $cat_posts = new WP_Query(
-		"showposts=" . $instance["num"] . 
-		"&cat=" . $instance["cat"] .
-		"&orderby=" . $sort_by .
-		"&order=" . $sort_order
-	  );
-
-		// Excerpt length filter
-		$new_excerpt_length = create_function('$length', "return " . $instance["excerpt_length"] . ";");
-		if ( $instance["excerpt_length"] > 0 )
-			add_filter('excerpt_length', $new_excerpt_length);
-		
-		echo $before_widget;
-		
-		// Widget title
-		echo $before_title;
-		if( isset( $instance["title_link"] ) )
-			echo '<a href="' . get_category_link($instance["cat"]) . '">' . $instance["title"] . '</a>';
-		else
-			echo $instance["title"];
-		echo $after_title;
-
-		// Post list
-		echo "<ul>\n";
-		
-		while ( $cat_posts->have_posts() )
-		{
-			$cat_posts->the_post();
-		?>
-			<li class="cat-post-item">
-				<a class="post-title" href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a>
-				
-
-				<?php if ( isset( $instance['date'] ) ) : ?>
-				<p class="post-date"><?php the_time("j M Y"); ?></p>
-				<?php endif; ?>
-				
-				<?php
-					if (
-						function_exists('the_post_thumbnail') &&
-						current_theme_supports("post-thumbnails") &&
-						isset( $instance["thumb"] ) &&
-						has_post_thumbnail()
-					) :
-				?>
-					<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
-					<?php the_post_thumbnail( 'cat_post_thumb_size'.$this->id ); ?>
-					</a>
-				<?php endif; ?>
-							
-				<?php if ( isset( $instance['excerpt'] ) ) : ?>
-				<?php the_excerpt(); ?> 
-				<?php endif; ?>
-				
-				<?php if ( isset( $instance['comment_num'] ) ) : ?>
-				<p class="comment-num">(<?php comments_number(); ?>)</p>
-				<?php endif; ?>
-			</li>
-			<?php
-		}
-		
-		echo "</ul>\n";
-		
-		echo $after_widget;
-
-		remove_filter('excerpt_length', $new_excerpt_length);
-		
-		wp_reset_postdata();
-	
-	}
-
-	/**
-	 * Update the options
-	 *
-	 * @param  array $new_instance
-	 * @param  array $old_instance
-	 * @return array
-	 */
-	function update($new_instance, $old_instance) {
-		$sizes = get_option('mkrdip_cat_post_thumb_sizes');
-			
-		if ( !$sizes ) {
-			$sizes = array();
-		}
-		
-		$sizes[$this->id] = array($new_instance['thumb_w'], $new_instance['thumb_h']);
-		update_option('mkrdip_cat_post_thumb_sizes', $sizes);
-				
-		return $new_instance;
-	}
-
-	/**
-	 * The widget configuration form back end.
-	 *
-	 * @param  array $instance
-	 * @return void
-	 */
-	function form($instance) {
-		$instance = wp_parse_args( ( array ) $instance, array(
-			'title'          => __( '' ),
-			'cat'			 => __( '' ),
-			'num'            => __( '' ),
-			'sort_by'        => __( '' ),
-			'asc_sort_order' => __( '' ),
-			'title_link'	 => __( '' ),
-			'excerpt'        => __( '' ),
-			'excerpt_length' => __( '' ),
-			'comment_num'    => __( '' ),
-			'date'           => __( '' ),
-			'thumb'          => __( '' ),
-			'thumb_w'        => __( '' ),
-			'thumb_h'        => __( '' )
-		) );
-
-		$title          = $instance['title'];
-		$cat 			= $instance['cat'];
-		$num            = $instance['num'];
-		$sort_by        = $instance['sort_by'];
-		$asc_sort_order = $instance['asc_sort_order'];
-		$title_link		= $instance['title_link'];		
-		$excerpt        = $instance['excerpt'];
-		$excerpt_length = $instance['excerpt_length'];
-		$comment_num    = $instance['comment_num'];
-		$date           = $instance['date'];
-		$thumb          = $instance['thumb'];
-		$thumb_w        = $instance['thumb_w'];
-		$thumb_h        = $instance['thumb_h'];
-				
-			?>
-			<p>
-				<label for="<?php echo $this->get_field_id("title"); ?>">
-					<?php _e( 'Title' ); ?>:
-					<input class="widefat" id="<?php echo $this->get_field_id("title"); ?>" name="<?php echo $this->get_field_name("title"); ?>" type="text" value="<?php echo esc_attr($instance["title"]); ?>" />
-				</label>
-			</p>
-			
-			<p>
-				<label>
-					<?php _e( 'Category' ); ?>:
-					<?php wp_dropdown_categories( array( 'name' => $this->get_field_name("cat"), 'selected' => $instance["cat"] ) ); ?>
-				</label>
-			</p>
-			
-			<p>
-				<label for="<?php echo $this->get_field_id("num"); ?>">
-					<?php _e('Number of posts to show'); ?>:
-					<input style="text-align: center;" id="<?php echo $this->get_field_id("num"); ?>" name="<?php echo $this->get_field_name("num"); ?>" type="text" value="<?php echo absint($instance["num"]); ?>" size='3' />
-				</label>
-			</p>
-
-			<p>
-				<label for="<?php echo $this->get_field_id("sort_by"); ?>">
-					<?php _e('Sort by'); ?>:
-					<select id="<?php echo $this->get_field_id("sort_by"); ?>" name="<?php echo $this->get_field_name("sort_by"); ?>">
-					  <option value="date"<?php selected( $instance["sort_by"], "date" ); ?>>Date</option>
-					  <option value="title"<?php selected( $instance["sort_by"], "title" ); ?>>Title</option>
-					  <option value="comment_count"<?php selected( $instance["sort_by"], "comment_count" ); ?>>Number of comments</option>
-					  <option value="rand"<?php selected( $instance["sort_by"], "rand" ); ?>>Random</option>
-					</select>
-				</label>
-			</p>
-			
-			<p>
-				<label for="<?php echo $this->get_field_id("asc_sort_order"); ?>">
-					<input type="checkbox" class="checkbox" 
-					  id="<?php echo $this->get_field_id("asc_sort_order"); ?>" 
-					  name="<?php echo $this->get_field_name("asc_sort_order"); ?>"
-					  <?php checked( (bool) $instance["asc_sort_order"], true ); ?> />
-							<?php _e( 'Reverse sort order (ascending)' ); ?>
-				</label>
-			</p>
-
-			<p>
-				<label for="<?php echo $this->get_field_id("title_link"); ?>">
-					<input type="checkbox" class="checkbox" id="<?php echo $this->get_field_id("title_link"); ?>" name="<?php echo $this->get_field_name("title_link"); ?>"<?php checked( (bool) $instance["title_link"], true ); ?> />
-					<?php _e( 'Make widget title link' ); ?>
-				</label>
-			</p>
-			
-			<p>
-				<label for="<?php echo $this->get_field_id("excerpt"); ?>">
-					<input type="checkbox" class="checkbox" id="<?php echo $this->get_field_id("excerpt"); ?>" name="<?php echo $this->get_field_name("excerpt"); ?>"<?php checked( (bool) $instance["excerpt"], true ); ?> />
-					<?php _e( 'Show post excerpt' ); ?>
-				</label>
-			</p>
-			
-			<p>
-				<label for="<?php echo $this->get_field_id("excerpt_length"); ?>">
-					<?php _e( 'Excerpt length (in words):' ); ?>
-				</label>
-				<input style="text-align: center;" type="text" id="<?php echo $this->get_field_id("excerpt_length"); ?>" name="<?php echo $this->get_field_name("excerpt_length"); ?>" value="<?php echo $instance["excerpt_length"]; ?>" size="3" />
-			</p>
-			
-			<p>
-				<label for="<?php echo $this->get_field_id("comment_num"); ?>">
-					<input type="checkbox" class="checkbox" id="<?php echo $this->get_field_id("comment_num"); ?>" name="<?php echo $this->get_field_name("comment_num"); ?>"<?php checked( (bool) $instance["comment_num"], true ); ?> />
-					<?php _e( 'Show number of comments' ); ?>
-				</label>
-			</p>
-			
-			<p>
-				<label for="<?php echo $this->get_field_id("date"); ?>">
-					<input type="checkbox" class="checkbox" id="<?php echo $this->get_field_id("date"); ?>" name="<?php echo $this->get_field_name("date"); ?>"<?php checked( (bool) $instance["date"], true ); ?> />
-					<?php _e( 'Show post date' ); ?>
-				</label>
-			</p>
-			
-			<?php if ( function_exists('the_post_thumbnail') && current_theme_supports("post-thumbnails") ) : ?>
-			<p>
-				<label for="<?php echo $this->get_field_id("thumb"); ?>">
-					<input type="checkbox" class="checkbox" id="<?php echo $this->get_field_id("thumb"); ?>" name="<?php echo $this->get_field_name("thumb"); ?>"<?php checked( (bool) $instance["thumb"], true ); ?> />
-					<?php _e( 'Show post thumbnail' ); ?>
-				</label>
-			</p>
-			<p>
-				<label>
-					<?php _e('Thumbnail dimensions (in pixels)'); ?>:<br />
-					<label for="<?php echo $this->get_field_id("thumb_w"); ?>">
-						Width: <input class="widefat" style="width:30%;" type="text" id="<?php echo $this->get_field_id("thumb_w"); ?>" name="<?php echo $this->get_field_name("thumb_w"); ?>" value="<?php echo $instance["thumb_w"]; ?>" />
-					</label>
-					
-					<label for="<?php echo $this->get_field_id("thumb_h"); ?>">
-						Height: <input class="widefat" style="width:30%;" type="text" id="<?php echo $this->get_field_id("thumb_h"); ?>" name="<?php echo $this->get_field_name("thumb_h"); ?>" value="<?php echo $instance["thumb_h"]; ?>" />
-					</label>
-				</label>
-			</p>
-			<?php endif; ?>
-
-			<?php
-
-		}
-
-}
-
-add_action( 'widgets_init', create_function('', 'return register_widget("CategoryPost");') );
-
-
-/**
- * Category Posts Widget Class
+ * Since everything within the plugin is registered via hooks,
+ * then kicking off the plugin from this point in the file does
+ * not affect the page life cycle.
  *
- * Makes a range of widget spaces and each one, or a range, is available to display newest items from categories or specific posts
+ * @since    1.0.0
  */
+function run_plugin_name() {
 
-class WidgetBox extends WP_Widget {
-	function WidgetBox() {
-		$widget_ops = array('classname' => 'widget-box', 'description' => __('A range of widgets to show on the home page - 3 x 3 box'));
-		$this->WP_Widget('category-posts', __('Category Posts'), $widget_ops);
-	}
+	$plugin = new Plugin_Name();
+	$plugin->run();
+
 }
-
-
-/**
- * Creation of the TagCloudShortcode
- * This class should host all the functionality that the plugin requires.
- */
-/*
- * first get the options necessary to properly display the plugin
- */
-
-
-
-if ( !class_exists( "TagCloudShortcode" ) ) {
-    
-    class TagCloudShortcode {
-
-        /**
-         * Shortcode Function
-         */
-         function shortcode($atts)
-         {
-
-      		$out = "";
-			$out .='<style type="text/css">div#tagcloud { margin-bottom: 50px; width: 90%; margin-left: auto; margin-right: auto; text-align: center; }</style>';
-
-			$out .= '<div id="tagcloud">';
-			
-			// do something intelligent to pull attributes to set up the parameters properly, with defaults. (not working yet. deal with it.)
-			$listparams = 'number=100&echo=0';
-			
-			$out .= wp_tag_cloud($listparams);
-			
-			$out .='</div>';
-            
-            return $out;
-         }
-    } // End Class TagCloudShortcode
-} 
-
-
-/**
- * Initialize the admin panel function 
- */
-
-if (class_exists("TagCloudShortcode")) {
-
-    $TagCloudShortcodeInstance = new TagCloudShortcode();
-}
-
-
-/**
-  * Set Actions, Shortcodes and Filters
-  */
-// Shortcode events
-if (isset($TagCloudShortcodeInstance)) {
-    add_shortcode('tagcloud',array(&$TagCloudShortcodeInstance, 'shortcode'));
-}
-
-
-/* Stop Adding Functions Below this Line */
-?>
+run_plugin_name();
